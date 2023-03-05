d179431abaff77c4a5c282bc6b29825ccb0acf82
viktorcus <carolynseglem@gmail.com>
2023-03-05 02:29:26 -0600

    Created FoodData and ActivityData TypeORM classes.

---
+++ src/entities/foodData.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class FoodData {
+
+    @PrimaryGeneratedColumn()
+    foodDataId: number;
+
+    @Column()
+    userId: number;
+
+    @CreateDateColumn()
+    mealDate: Date;
+
+    @Column()
+    meal: string;
+
+    @Column()
+    calorieIntake: number;
+
+    @Column()
+    note: string;
+}
