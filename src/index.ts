699a332fef542c5f29abb22d41d8a90b68b6b2a9
Thetrishna24 <trishna.niraula@gmail.com>
2023-03-15 20:33:54 -0500

    Added Update and Delete function

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import 'express-async-errors'; // Enable default error handling for async errors
 import { getAllUserFoodData, submitFoodData } from './controllers/FoodDataController';
 import { getAllUserActivityData, submitActivityData } from './controllers/ActivityDataController';
 import { getAllUserSleepData, addSleepData } from './controllers/SleepDataController';
+import { getAllSleepDataForUser } from './models/SleepDataModel';
+import { getAllUserHealthData } from './controllers/HealthDataController';
+import { getAllHealthDataForUser, updateHealthData } from './models/HealthDataModel';
 
 const app: Express = express();
 app.use(express.json());
@@ -16,7 +19,11 @@ app.get('/api/activity/:userId', getAllUserActivityData);
 app.post('/api/activity', submitActivityData);
 
 app.get('/api/sleep/:userId', getAllUserSleepData);
-app.post('/api/sleep', addSleepData);
+app.post('/api/activity/', getAllSleepDataForUser);
+
+app.get('/api/activity/:userId', getAllUserHealthData);
+app.get('/api/activity/:userId', getAllHealthDataForUser);
+app.post('/api/activity/', updateHealthData);
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
