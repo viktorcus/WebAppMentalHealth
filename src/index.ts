57db0a853635acef20c615cd96e169868b67d438
vynguyn <vy21014nguyn@gmail.com>
2023-04-12 11:59:34 -0500

    Update files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import 'express-async-errors'; // Enable default error handling for async errors
 import express, { Express } from 'express';
 import session from 'express-session';
 import connectSqlite3 from 'connect-sqlite3';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
+
 import {
   registerUser,
   logIn,
