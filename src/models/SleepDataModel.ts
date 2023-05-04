e56c9d99e2b207c46a0320c74c2d9a23dd64c25b
Thetrishna24 <trishna.niraula@gmail.com>
2023-05-04 09:25:17 -0500

    Updated Changes

--- src/models/SleepDataModel.ts
+++ src/models/SleepDataModel.ts
@@ -1,11 +1,12 @@
 import { AppDataSource } from '../dataSource';
 import { SleepData } from '../entities/sleepData';
+import { User } from '../entities/user';
 
 const sleepRepository = AppDataSource.getRepository(SleepData);
 
-async function addSleepData(sleepData: SleepData): Promise<SleepData> {
+async function addSleepData(sleepData: SleepData, user: User): Promise<SleepData> {
   let newSleepData = new SleepData();
-  newSleepData.userId = sleepData.userId;
+  newSleepData.user = user;
   newSleepData.sleepDate = sleepData.sleepDate;
   newSleepData.hoursSlept = sleepData.hoursSlept;
   newSleepData.quality = sleepData.quality;
@@ -29,14 +30,16 @@ async function getSleepDataById(sleepDataId: string): Promise<SleepData | null> 
   return sleepData || null;
 }
 
-async function updateSleepData(sleepDataId: string, sleepData: SleepData): Promise<SleepData | null> {
+async function updateSleepData(
+  sleepDataId: string,
+  sleepData: SleepData
+): Promise<SleepData | null> {
   let updatedSleepData = await getSleepDataById(sleepDataId);
 
   if (!updatedSleepData) {
     return null;
   }
 
-  updatedSleepData.userId = sleepData.userId;
   updatedSleepData.sleepDate = sleepData.sleepDate;
   updatedSleepData.hoursSlept = sleepData.hoursSlept;
   updatedSleepData.quality = sleepData.quality;
@@ -68,6 +71,50 @@ async function getSleepDataByDateRange(
     .getMany();
 }
 
+async function generateSleepStats(
+  userId: string,
+  start: Date,
+  end: Date
+): Promise<SleepDataStats[]> {
+  // will also include userid once session management is in place
+  const sleep: SleepData[] = await sleepRepository
+    .createQueryBuilder('sleepData')
+    .where('sleepData.user.userId = :userId and sleepDate >= :start and sleepDate <= :end', {
+      userId,
+      start,
+      end,
+    })
+    .getMany();
+
+  const stats: SleepDataStats[] = [];
+  for (const data of sleep) {
+    const idx = stats.findIndex(
+      (s) =>
+        s.date.getFullYear() === data.sleepDate.getFullYear() &&
+        s.date.getMonth() === data.sleepDate.getFullYear() &&
+        s.date.getDate() === data.sleepDate.getDate()
+    );
+    if (idx >= 0) {
+      const sleepStat = stats.at(idx);
+      if (sleepStat?.hours) {
+        sleepStat.hours += data.hoursSlept;
+      }
+    } else {
+      stats.push({
+        date: new Date(
+          data.sleepDate.getFullYear(),
+          data.sleepDate.getMonth(),
+          data.sleepDate.getDate()
+        ),
+        hours: data.hoursSlept,
+        quality: data.quality,
+        note: data.note,
+      });
+    }
+  }
+  return stats.sort((a, b) => a.date.valueOf() - b.date.valueOf());
+}
+
 export {
   addSleepData,
   getAllSleepDataForUser,
@@ -75,4 +122,5 @@ export {
   updateSleepData,
   deleteSleepData,
   getSleepDataByDateRange,
+  generateSleepStats,
 };
